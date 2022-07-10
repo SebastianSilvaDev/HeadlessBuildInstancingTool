ea7667c68eb5fb943a1c4188eb59f6f6bab0586d
Fireinfern <sebastiansilvabenites@gmail.com>
2022-07-10 10:17:22 -0500

    Add Clap command line

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 use std::process::{Child, Command};
 use sysinfo::{ProcessExt, System, SystemExt};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    #[clap(short, long, value_parser)]
+    executable_name: String,
+    #[clap(short='i', long, value_parser, default_value="20")]
+    number_of_instances: i32,
+    #[clap(long="nullRHI")]
+    null_rhi: bool,
+    #[clap(short='s', long, value_parser, default_value="20")]
+    time_to_stop: u64,
+    #[clap(long="wait-time", value_parser)]
+    time_between_executions: Option<u64>
+}
 
 pub fn execute(exe: &str, args: &[&str]) -> Child {
     let command = Command::new(exe).args(args).spawn().expect("cant spawn");
@@ -7,7 +23,9 @@ pub fn execute(exe: &str, args: &[&str]) -> Child {
 }
 
 fn main() {
-    let executable_name = std::env::args().nth(1).expect("No executable given!");
+    let args: Args = Args::parse();
+
+    let executable_name = args.executable_name;
     let mut path: String = String::from("./");
     path.push_str(&executable_name);
     path.push_str(".exe");
@@ -15,28 +33,23 @@ fn main() {
         println!("Path does not exists!");
         return;
     }
-    let times = std::env::args()
-        .nth(2)
-        .expect("No variable times has been created");
-    let numer_times: i32 = times.parse().unwrap();
+    let numer_times: i32 = args.number_of_instances;
+    let mut execution_args: Vec<&str> = Vec::new();
+    if args.null_rhi {
+        execution_args.push("-nullRHI");
+    }
     println!("Runing {numer_times} times");
     let mut children: Vec<Child> = Vec::new();
     for _i in 1..(numer_times + 1) {
-        let child_result = execute(&path, &["-nullRHI"]);
+        let child_result = execute(&path, &execution_args);
         children.push(child_result);
         println!("Process {_i} Spawned!");
+        if args.time_between_executions.is_some() {
+            std::thread::sleep(std::time::Duration::from_secs(args.time_between_executions.unwrap()));
+        }
     }
     println!("Finish spawning!");
-    let wait_time_to_stop = std::env::args().nth(3);
-    let mut sleep_time: u64 = 20;
-    match wait_time_to_stop {
-        Some(x) => {
-            // Implicit parse to u64
-            sleep_time = x.parse().unwrap();
-            println!("Time to wait is {sleep_time} seconds");
-        },
-        None => println!("The time will be 20"),
-    }
+    let sleep_time: u64 = args.time_to_stop;
     std::thread::sleep(std::time::Duration::from_secs(sleep_time));
     println!("Finish Sleeping!");
     while !children.is_empty() {
